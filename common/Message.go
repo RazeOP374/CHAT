a7d846426a04c8e4b86d2e18f6b46e977334e9f0
LOVE SUISEI <87218053+Raze9@users.noreply.github.com>
2022-12-08 16:24:37 +0800

    Add files via upload

---
+++ common/Message.go
@@ -0,0 +1,24 @@
+package common
+
+const (
+	Loginmestype    = "Loginmes"
+	LoginResmestype = "LoginResmes"
+	RegisterMestype = "RegisterMes"
+)
+
+type Message struct {
+	Type string `json:"type"`
+	Data string `json:"data"`
+}
+type Loginmes struct {
+	Userid   int    `json:"userid"`
+	Userpwd  string `json:"userpwd"`
+	UserName string `json:"userName"`
+}
+type LoginResmes struct {
+	Code  int    `json:"code"`
+	Error string `json:"error"`
+}
+type RegisterMes struct {
+	registerpwd int `json:"registerpwd"`
+}
